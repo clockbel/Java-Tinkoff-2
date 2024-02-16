aa34c306d4256298170d124b36042caf5eca2f58
kamil <mubarakov.cail@yandex.ru>
2024-02-16 21:07:48 +0300

    checkstyle dto 0.1

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -17,19 +17,20 @@ import reactor.core.publisher.Mono;
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(ScrapperApplication.class);
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(ScrapperApplication.class, args);
         GitHubClient gitHubClient = context.getBean(GitHubClient.class);
         Mono<RepositoryResponse> userResponseMono = gitHubClient.fetchRepository("clockbel", "Java-Tinkoff");
         userResponseMono.subscribe(user -> {
-            System.out.println("Репозиторий: " + user.getFullName() + "Дата создания: " + user.getCreatedAt());
+            LOGGER.info("Репозиторий: " + user.getFullName() + "Дата создания: " + user.getCreatedAt());
         });
         StackOverflowClient stackOverflowClient = context.getBean(StackOverflowClient.class);
         @SuppressWarnings("MagicNumber")
         long questionId = 1566516;
         Mono<QuestionResponse> questionResponseMono = stackOverflowClient.fetchQuestion(questionId);
         questionResponseMono.subscribe(questionResponse -> {
-            System.out.println("Номер вопроса: " + questionResponse.getItems().get(0).getQuestionId());
+            LOGGER.info("Номер вопроса: " + questionResponse.getItems().get(0).getQuestionId());
         });
     }
 }
