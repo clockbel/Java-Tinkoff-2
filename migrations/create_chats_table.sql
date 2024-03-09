e7c805b4a7e8e1e46cf4ee22f546956271eb0719
kamil <mubarakov.cail@yandex.ru>
2024-03-10 00:07:13 +0300

    db 1

---
+++ migrations/create_chats_table.sql
@@ -0,0 +1,5 @@
+create table if not exists chat(
+	id bigint not null,
+	created_at timestamp with time zone not null,
+	primary key(id)
+);
---
+++ migrations/create_links_table.sql
@@ -0,0 +1,8 @@
+create table if not exists link (
+    id bigint generated always as identity,
+    url text not null,
+    last_updated timestamp with time zone not null,
+    primary key (id)
+);
+
+
